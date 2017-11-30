827a1710de12ccc45ba1d7984a2f7552164ae2d3
Bohee Park <boheepark@yahoo.com>
2017-11-30 02:09:42 -0500

    react

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(os.getenv("APP_SETTINGS"))
     db.init_app(app)
 
