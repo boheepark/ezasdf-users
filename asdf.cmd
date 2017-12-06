4e069a9d7508a0bda870bd5537185aa182cf2dfc
Bohee Park <boheepark91@gmail.com>
2017-12-06 06:02:30 -0500

    staging

---
+++ asdf.cmd
@@ -0,0 +1,7 @@
+SET APP_SETTINGS=project.config.DevelopmentConfig
+SET DATABASE_URL=postgres://postgres:postgres@localhost:5432/users_dev
+SET DATABASE_TEST_URL=postgres://postgres:postgres@localhost:5432/users_test
+SET SECRET_KEY=342d296040947a965aeeb9cf1080489a3afaf66743db6834
+
+REM set REACT_APP_USERS_SERVICE to docker ip
+REM set TEST_URL to docker ip for testcafe
