cefba311d49d09076535d93e67c579082a51acb6
Bohee Park <boheepark91@gmail.com>
2017-12-06 03:51:43 -0500

    env

--- env.cmd
+++
@@ -1,4 +0,0 @@
-SET APP_SETTINGS=project.config.DevelopmentConfig
-SET DATABASE_URL=postgres://postgres:postgres@localhost:5432/users_dev
-SET DATABASE_TEST_URL=postgres://postgres:postgres@localhost:5432/users_test
-SET SECRET_KEY=secret
