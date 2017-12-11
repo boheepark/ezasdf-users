c3f9573b89053a968fd9bb0d2b5203cd985975ef
Bohee Park <boheepark91@gmail.com>
2017-12-11 04:20:55 -0500

    refactor

--- manage.py
+++ manage.py
@@ -21,29 +21,19 @@ manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def cov():
-    """ Runs the unit tests with coverage. """
-
-    tests = unittest.TestLoader().discover('project/tests')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        COV.stop()
-        COV.save()
-        print('Coverage Summary:')
-        COV.report()
-        COV.html_report()
-        COV.erase()
-        return 0
-    return 1
-
-
-@manager.command
-def test():
+def test(cov=False):
     """ Runs the unit tests without coverage. """
 
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        if cov:
+            COV.stop()
+            COV.save()
+            print('Coverage Summary:')
+            COV.report()
+            COV.html_report()
+            COV.erase()
         return 0
     return 1
 
